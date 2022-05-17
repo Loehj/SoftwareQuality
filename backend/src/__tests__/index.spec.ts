aa10802de355e8d177c6774d92d608e1d05f5da1
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2022-05-17 15:40:08 +0200

    chore: add vitepress

--- backend/src/__tests__/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { getMessage } from "..";
-
-describe("Hello World", () => {
-  it("should return message", () => {
-    const msg = getMessage();
-    expect(msg).toBe("Hallo von der Softwarequalität API.");
-  });
-});
---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "Softwarequalität INF19B - Softwaretest und CI/CD",
+  themeConfig: {
+    logo: "logo-dhbw.png",
+    sidebar: [
+      {
+        text: "Git ohne Pipeline",
+        children: [
+          {
+            text: "Risiken und Lösungen",
+            link: "/ohne-pipeline/risiken-und-loesungen",
+          },
+        ],
+      },
+      {
+        text: "Git mit  CI/CD-Pipeline",
+        children: [
+          {
+            text: "Aufbau der Pipeline",
+            link: "/mit-pipeline/aufbau",
+          },
+          {
+            text: "Stages",
+            link: "/mit-pipeline/stages",
+          },
+          {
+            text: "Vor- und Nachteile",
+            link: "/mit-pipeline/vor-und-nachteile",
+          },
+        ],
+      },
+    ],
+  },
+});
